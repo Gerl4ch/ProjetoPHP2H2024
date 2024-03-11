e063b9ac2cff09dd0b6587e51367a6a59a306ca6
leonardo <leonardohg2005@gmail.com>
2024-03-11 20:16:06 -0300

    trabalhando com escopo de função e script

---
+++ exemplophp05.php
@@ -0,0 +1,15 @@
+<?php 
+
+    $nome = "Armandino";
+
+    function exibir(){
+    $nome = "Josefina";
+
+    echo "Valor da variável dentro da função " . $nome;
+    }
+
+    exibir();
+
+    echo "<br /> Valor de variável fora da função " . $nome;
+
+ ?>
