94846733d8e0066980d1cd8213aa5a20cde3eb6a
leonardo <leonardohg2005@gmail.com>
2024-03-11 19:49:23 -0300

    exemplo 04

---
+++ exemplophp04.php
@@ -0,0 +1,15 @@
+<?php 
+
+     $valor1 = 50.00;
+     $valor2 = 30.50;
+
+     $soma = $valor1 + $valor2;
+
+     echo "A soma de $valor1 mais $valor2 é $soma <br />";
+
+     echo 'A soma de $valor1 mais $valor2 é $soma <br />';
+
+     echo 'A soma de ' . $valor1 . ' mais ' . $valor2 . ' é ' .$soma . '<br />';
+
+
+ ?>
