583f0061e25457b960f3db2f4f13da537480fcbd
leonardo <leonardohg2005@gmail.com>
2024-03-20 22:12:56 -0300

    criando funções

---
+++ calculadora.php
@@ -0,0 +1,21 @@
+<?php 
+
+function calcular ($num1,$num2){
+    return ($num1 + $num2);
+}
+
+function calcular2 ($num1,$num2){
+    return ($num1 - $num2);
+}
+
+function calcular3 ($num1,$num2){
+	return ($num1 * $num2);
+}
+
+function calcular4 ($num1,$num2){
+	return ($num1 / $num2);
+}
+echo calcular (10,20);
+
+echo calcular2 (10,20);
+ ?>
---
+++ exemplophp08.php
@@ -0,0 +1,9 @@
+<?php 
+
+function calcular ($num1,$num2){
+return ($num1 + $num2);
+}
+
+echo calcular (10,20);
+
+ ?>
